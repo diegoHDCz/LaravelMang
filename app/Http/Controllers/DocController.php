d1ceca50fbbe9ee66d47586b95e8ffb6fa27a7da
Diego Czajka <diego.czajka@gmail.com>
2021-03-25 12:50:08 -0300

    storage document

--- app/Http/Controllers/DocController.php
+++ app/Http/Controllers/DocController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Doc;
+use Illuminate\Support\Facades\Storage;
 
 class DocController extends Controller
 {
-    //
+    public function getAll() {
+        $array = ['error'=>''];
+
+        $docs = Doc::all();
+
+        foreach($docs as $docKey => $docValue) {
+            $docs[$docKey]['fielurl'] = asset('storage/'.$docValue['fileurl']);
+        }
+
+        $array['list'] =$docs;
+
+        return $array;
+    }
 }
