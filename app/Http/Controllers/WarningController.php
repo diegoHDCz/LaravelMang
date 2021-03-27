d770e564a60d4e4b8c19b030dd6eb5f2c31d5847
diegoHDCz <diego.czajka@gmail.com>
2021-03-27 16:45:52 -0300

    Warnings

--- app/Http/Controllers/WarningController.php
+++ app/Http/Controllers/WarningController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Warning;
+use App\Models\Unit;
 class WarningController extends Controller
 {
-    //
+    public function getMyWarnings(Request $request) {
+        $array = ['error'=>''];
+
+        $property = $request->input('property');
+
+        if($property) {
+            $user = auth()->user();
+            
+            $unit = Unit::where('id', $property)
+            ->where('id_owner', $user['id'])
+            ->count();
+
+            if($unit > 0){
+
+                $warnings = Warning::where('id_unit', $property)
+                ->orderBy('datecreated', 'DESC')
+                ->orderBy('id', 'DESC')
+                ->get();
+
+                $array['list'] = $warnings;
+
+            }else{
+                $array['error'] = 'Esta unidade não é sua.';
+            }
+
+        }else{
+            $array['error'] = 'A propriedade é necessária';
+        }
+        
+
+        return $array;
+    }
 }
